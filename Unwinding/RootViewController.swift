b354362bd5161992dfd29e69aff4a7cbc2793c20
Bart Jacobs <developer@bartjacobs.com>
2016-07-27 07:17:13 +0200

    Implement unwind action in RootViewController

--- Unwinding/RootViewController.swift
+++ Unwinding/RootViewController.swift
@@ -10,26 +10,16 @@ import UIKit
 
 class RootViewController: UIViewController {
 
+    // MARK: - View Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+    // MARK: - Actions
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func unwindToRootViewController(segue: UIStoryboardSegue) {
+        print("Unwind to Root View Controller")
     }
-    */
 
 }
